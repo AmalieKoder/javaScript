b6d5b339915cd75c60a18b2c9bbc840fb35781d3
AmalieKoder <amlie.koder@gmail.com>
2023-10-05 09:42:32 +0200

    javascript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Hello World");
 
+// Hente HTML element
+
 const plusButton = document.querySelector("#plusButtom");
 console.log(plusButton);
 
@@ -10,9 +12,11 @@ const numberDisplay = document.querySelector("#displayNumber");
 
 const resetButton = document.querySelector("#resetButton");
 
+// Vi må sette opp variabler som rekner
 
 let count = 0;
 
+// Lage funksjoner til knappene
 
 function increment() {
     count = count + 1;
@@ -30,6 +34,8 @@ function reset() {
     count = 0;
     numberDisplay.textContent = count;
 }
+
+// Denne event lytteren tar en allerede laget funksjon som sitt andre parament
 
 plusButton.addEventListener("click", increment);
 
